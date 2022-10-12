fdf2a86f4f2349d3d1cdc772e5e95c75b88c4a8d
alinatamkevich <atomkevich@gmail.com>
2022-10-13 00:16:04 +0300

    add model

--- train/task/task_executor.py
+++ train/task/task_executor.py
@@ -103,7 +103,8 @@ class TaskRunner:
         model_input_fields = self.model_input_fields
         input = [data[key] for key in model_input_fields]
         input = [i.to(self.device) for i in input]
-        print(self.device, input[0].get_device(), input[1].get_device())
+        warnings.warn(self.device.type)
+
 
         if len(input) != 1:
             warnings.warn("make sure your model has list of inputs")
