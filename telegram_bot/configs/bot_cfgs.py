a8cd3c12ef26934f1c81b4d412b980b868f14431
art <monkeyking1984@yandex.ru>
2022-10-23 17:08:46 +0300

    consumer without config

--- telegram_bot/configs/bot_cfgs.py
+++ telegram_bot/configs/bot_cfgs.py
@@ -24,3 +24,27 @@ consumer_msg_cfd = KafkaConsumerCfg(
                                         processes=2
                                         )
 
+
+consumer_msg_cfd = KafkaConsumerCfg(
+                                        kafka_topic='my-topic',
+                                        auto_offset_reset="earliest",
+                                        enable_auto_commit=False,
+                                        bootstrap_servers=['localhost:9092'],
+                                        group_id='my-group',
+                                        client_id='client1',
+                                        check_crcs=True, # defult value True
+                                        # consumer_timeout_ms=[float('inf')],  # defult value [float('inf')]
+                                        session_timeout_ms=10000,  # defult value 1000 ms
+                                        request_timeout_ms=305000,  # defult value  305000 ms
+                                        max_poll_interval_ms=300000 * 10,  # defult value  300000 ms
+                                        max_partition_fetch_bytes=1048576,  # defult value  1048576 bytes
+                                        max_poll_records=1000,  # defult value 500
+                                        value_deserializer=lambda v: json.loads(v.decode('ascii')),
+                                        key_deserializer=lambda v: json.loads(v.decode('ascii')),
+                                        #### for main_loop method witch don't working properly
+                                        pool_cache_limit=1,
+                                        stop_processing = False,
+                                        processes=2
+                                        )
+
+
