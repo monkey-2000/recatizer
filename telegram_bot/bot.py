c8d143b5b4572bf96239bb7b29bd7c9bc4ff93f5
alinatamkevich <atomkevich@gmail.com>
2022-11-11 19:39:37 +0300

    add consumer - producer

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -2,7 +2,7 @@ import os
 import uuid
 
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.contrib.fsm_storage.mongo import MongoStorage
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import StatesGroup, State
@@ -11,8 +11,7 @@ from telegram_bot.queu.producer import Producer
 
 
 bot = Bot(token=os.environ.get('BOT_TOKEN'))
-mongodb_name = "answer_question_aiogram"
-storage = MongoStorage(db_name=mongodb_name)
+storage = MemoryStorage()
 kafka_producer = Producer()
 dp = Dispatcher(bot, storage=storage)
 class QStates(StatesGroup):
@@ -21,12 +20,12 @@ class QStates(StatesGroup):
     geo = State()
 
 @dp.message_handler(commands=['start'], state="*")
-async def main_menu(message: types.Message, state: FSMContext):
+async def start(message: types.Message, state: FSMContext):
     await state.reset_state(with_data=False)
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     buttons = []
-    user_data = await state.get_data()
-    print(user_data)
+    #user_data = await state.get_data()
+    #print(user_data)
     buttons.append(types.KeyboardButton(text="I saw a cat"))
     buttons.append(types.KeyboardButton(text="I lost my cat"))
     keyboard.add(*buttons)
@@ -34,17 +33,17 @@ async def main_menu(message: types.Message, state: FSMContext):
 
 
 @dp.message_handler(Text(equals="I lost my cat", ignore_case=True))
-async def ask_age(message: types.Message):
+async def lost_cat(message: types.Message, state: FSMContext):
     await message.answer("Please upload photo of your cat",
                          reply_markup=types.ReplyKeyboardRemove())
-    await QStates.find.set()
+    await state.set_state(QStates.find)
 
 
 @dp.message_handler(Text(equals="I saw a cat", ignore_case=True))
-async def ask_age(message: types.Message):
+async def saw_cat(message: types.Message, state: FSMContext):
     await message.answer("Please upload photo of cat",
                          reply_markup=types.ReplyKeyboardRemove())
-    await QStates.saw.set()
+    await state.set_state(QStates.saw)
 
 def to_message(user_id: str, image_path: str, additional_info: str, quadkey: str = ""):
     return {'user_id': user_id,
@@ -53,22 +52,24 @@ def to_message(user_id: str, image_path: str, additional_info: str, quadkey: str
                  'quadkey': quadkey}
 
 @dp.message_handler(state=QStates.find, content_types=['photo'])
-async def enter_age(message: types.Message, state: FSMContext):
-    additional_info = message.md_text
+async def process_find(message: types.Message, state: FSMContext):
+    additional_info = message.to_python().get("caption", "")
     id = str(uuid.uuid4())
     image_name = "{0}.jpg".format(id)
-    image_path = os.path.join(bot_config.image_dir, image_name)
+    os.makedirs("/tmp/data", exist_ok=True)
+    image_path = os.path.join("/tmp/data", image_name)
     await message.photo[-1].download(image_path)
     kafka_message = to_message(message.from_user.id, image_path, additional_info)
     kafka_producer.send(value=kafka_message, key=id, topic='find_cat')
     await message.answer("Thanks! We notify you when we'll get any news")
 
 @dp.message_handler(state=QStates.saw, content_types=['photo'])
-async def enter_sex(message: types.Message, state: FSMContext):
-    additional_info = message.md_text
+async def process_saw(message: types.Message, state: FSMContext):
+    additional_info = message.to_python().get("caption", "")
     id = str(uuid.uuid4())
     image_name = "{0}.jpg".format(id)
-    image_path = os.path.join(bot_config.image_dir, image_name)
+    os.makedirs("/tmp/data", exist_ok=True)
+    image_path = os.path.join("/tmp/data", image_name)
     await message.photo[-1].download(image_path)
     kafka_message = to_message(message.from_user.id, image_path, additional_info)
     kafka_producer.send(value=kafka_message, key=id, topic='saw_cat')
--- telegram_bot/index.py
+++ telegram_bot/index.py
@@ -1,11 +1,37 @@
-import telebot
-from bot import dp
+"""
+Simple echo Telegram Bot example on Aiogram framework using
+Yandex.Cloud functions.
+"""
+
+import asyncio
+import json
+import logging
+import os
+from aiogram import Bot, Dispatcher, types
+from telegram_bot.bot import dp
+
+# Logger initialization and logging level setting
+log = logging.getLogger(__name__)
+log.setLevel(os.environ.get('LOGGING_LEVEL', 'INFO').upper())
+
+async def process_event(event, dp: Dispatcher):
+    """
+    Converting an Yandex.Cloud functions event to an update and
+    handling tha update.
+    """
+
+    update = json.loads(event['body'])
+    Bot.set_current(dp.bot)
+    log.debug('Update: ' + str(update))
+    update = types.Update.to_object(update)
+    await dp.process_update(update)
 
 
-def handler(event, _):
-    message = telebot.types.Update.de_json(event['body'])
-    dp.process_new_updates([message])
-    return {
-        'statusCode': 200,
-        'body': '!',
-    }
+async def handler(event, context):
+    """Yandex.Cloud functions handler."""
+
+    if event['httpMethod'] == 'POST':
+        await process_event(event, dp)
+
+        return {'statusCode': 200, 'body': 'ok'}
+    return {'statusCode': 405}
