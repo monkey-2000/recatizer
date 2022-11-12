e6068328578b22b3fe5b11553af26f7e562b725f
alinatamkevich <atomkevich@gmail.com>
2022-11-12 10:54:17 +0300

    add consumer - producer

--- webhook_remove.py
+++ webhook_remove.py
@@ -3,4 +3,4 @@ import telebot
 bot = telebot.TeleBot("5725782396:AAHCjlA4YKa0YlPudMBRNsWI1nEtEOClI5w")
 
 bot.remove_webhook()
-bot.set_webhook("https://functions.yandexcloud.net/d4ei53dob158ltqvood2")
+bot.set_webhook("https://d5dogtbmo276i8mgmg42.apigw.yandexcloud.net")
