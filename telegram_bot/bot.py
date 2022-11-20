f81a59edf9b7e5b2200584f97b608ca3a76da09e
Victor <vzouga@gmail.com>
2022-11-20 20:44:18 +0400

    Update bot.py

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+import mercantile
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -47,6 +48,29 @@ async def saw_cat(message: types.Message, state: FSMContext):
     await message.answer("Please upload photo of cat",
                          reply_markup=types.ReplyKeyboardRemove())
     await state.set_state(QStates.saw)
+
+def get_keyboard():
+    keyboard = types.ReplyKeyboardMarkup()
+    button = types.KeyboardButton("Share Position", request_location=True)
+    keyboard.add(button)
+    return keyboard
+
+@dp.message_handler(content_types=['location'])
+async def handle_location(message: types.Message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+    reply = "latitude:  {}\nlongitude: {}".format(lat, lon)
+    await message.answer(reply, reply_markup=types.ReplyKeyboardRemove())
+
+@dp.message_handler(commands=['locate_me'])
+async def cmd_locate_me(message: types.Message):
+    reply = "Click on the the button below to share your location"
+    await message.answer(reply, reply_markup=get_keyboard())
+
+def point_to_quadkey(lon: float, lat: float, zoom: int) -> str:
+    tile = mercantile.tile(lon, lat, zoom)
+
+    return mercantile.quadkey(tile)
 
 def to_message(user_id: str, image_path: str, additional_info: str, quadkey: str = ""):
     return {'user_id': user_id,
