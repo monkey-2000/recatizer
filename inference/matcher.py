28dd3527f128cd2aeb548b8b49b4b033eb7a832f
alinatamkevich <atomkevich@gmail.com>
2022-11-22 23:43:10 +0300

    add onnx and openvino support

--- inference/matcher.py
+++ inference/matcher.py
@@ -20,8 +20,7 @@ class Predictor:
         self.s3_client = YandexS3Client(s3_config.aws_access_key_id, s3_config.aws_secret_access_key)
         self.s3_client.download_file(models_path, local_model_path)
         self.model = CatIrClassificator(local_model_path)
-    def _download_model(self, path: str):
-        self.s3_client.download_file(path)
+
     def _images_to_tensor(self, img):
         """ As input is image, img always comes in channels_last format """
         shape = img.shape
