dcad5b897783f7c46c7a63f9ed6e1d52961f2461
art <monkeyking1984@yandex.ru>
2022-12-17 11:31:20 +0300

    deploy

--- src/configs/service_config.py
+++ src/configs/service_config.py
@@ -32,7 +32,7 @@ default_service_config = ServiceConfig(
     ),
     models_path=os.environ.get("MODEL_PATH"),
     local_models_path=os.environ.get("LOCAL_MODEL_PATH"),
-    answer_time_delay=120,  ## dely time for bot loader (one cat  in 5 sec)
-    ans_check_frequency=10, ## how often do we check for answers in main _nference_loop
+    answer_time_delay=3600,  ## dely time for bot loader (one cat  in 5 sec)
+    ans_check_frequency=1800, ## how often do we check for answers in main _nference_loop
     cats_in_answer=5
 )
