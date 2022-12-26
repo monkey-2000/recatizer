5d3d1a8b4bb58f19329aa4b641a6bd56b3b9d5d3
art <monkeyking1984@yandex.ru>
2022-12-26 19:11:36 +0300

    text edited

--- src/telegram_bot/bot_tools/menu_texts.py
+++ src/telegram_bot/bot_tools/menu_texts.py
@@ -1,8 +1,7 @@
 from src.telegram_bot.configs.bot_cfgs import bot_config
 
-start_menu_text = "Hello to Recatizer.\n" \
-                   "LOST CAT - click if you lost your cat.\n' \
-                    'SAW CAT - Сlick if you think you have seen a lost cat."
+start_menu_text = "Hello to Recatizer. Click:\n" \
+                   "LOST CAT -  if you lost your cat.\nSAW CAT - if you think you have seen a lost cat."
 
 saw_lost_menu_text =  f'Please upload photo of cat (limit {bot_config.max_load_photos})'
 
@@ -14,7 +13,7 @@ help_text = '~~~~~~~It s Recatizer v0~~~~~~~~~\n' \
                   'LOST CAT - click if you lost your cat. ' \
                   'We will send you the most similar ones in your area.\n' \
                   '~ You can only look for one cat.\n' \
-                  '~ Mark all matches \U0000274c or \U0001F638.' \
+                  '~ Mark all matches \U0000274c or \U00002705.' \
                   'So we can understand you better. \n' \
                   '~ If you need to look for another cat then click:\n ' \
                   'LOST CAT ---- > Unsubscribe ---- > LOST CAT\n\n' \
